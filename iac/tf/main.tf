8bd36d2dc50289cb69a1cdc0fa381cb2caa36813
Mohammadabbas Merchant <mohammadabbas.me@tigeranalytics.local>
2021-03-08 12:29:25 +0530

    Changed variable and added dataset

--- iac/tf/main.tf
+++ iac/tf/main.tf
@@ -1,29 +1,29 @@
 
+#terraform import azurerm_resource_group.adf_resource_test /subscriptions/8cbfc19a-ed09-496d-8b87-3f3a46f7b9a5/resourceGroups/resource_group_mohammad
 resource "azurerm_resource_group" "adf_resource_test" {
-  name     = "${var.name}_resources_test"
-  location = "West Europe"
+ name     = var.resource_group_name
+ location = var.location
 }
-
 
 # Azure Data factory resource.
 resource "azurerm_data_factory" "adf_data_factory_test1" {
-	name = "${var.name}-data-factory-test1"
-	location = azurerm_resource_group.adf_resource_test.location
-	resource_group_name = azurerm_resource_group.adf_resource_test.name
+    name = var.data_factory_name
+    location = azurerm_resource_group.adf_resource_test.location
+    resource_group_name = azurerm_resource_group.adf_resource_test.name
 
-	github_configuration{
-		account_name = "mohammad-abbas-me"
-		branch_name = "adf_dev"
-		git_url = "https://github.com"
-		repository_name = "adf_automate"
-		root_folder = "/adf_artifacts"
-	}
+    github_configuration{
+        account_name = var.github_account_name
+        branch_name = var.github_branch_name
+        git_url = var.github_git_url
+        repository_name = var.github_repo_name
+        root_folder = var.github_root_dir
+    }
 }
 
 # ===================
 # Blob storage.
 resource "azurerm_storage_account" "adf_storage_account_test_1" {
-  name                     = "${var.name}blobstoragetest1"
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.adf_resource_test.name
   location                 = azurerm_resource_group.adf_resource_test.location
   account_tier             = "Standard"
@@ -31,13 +31,13 @@ resource "azurerm_storage_account" "adf_storage_account_test_1" {
 }
 
 resource "azurerm_storage_container" "adf_storage_container_test" {
-  name                  = "${var.name}storagecontainertest"
+  name                  = var.storage_container_name
   storage_account_name  = azurerm_storage_account.adf_storage_account_test_1.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "adf_storage_blob_test" {
-  name                   = "${var.name}_storage_blob_test.zip"
+  name                   = var.storage_blob_name
   storage_account_name   = azurerm_storage_account.adf_storage_account_test_1.name
   storage_container_name = azurerm_storage_container.adf_storage_container_test.name
   type                   = "Block"
@@ -47,7 +47,7 @@ resource "azurerm_storage_blob" "adf_storage_blob_test" {
 
 # Linked blob storage with data factory
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "adf_linked_storage_test" {
-  name                = "${var.name}_linked_storage_test"
+  name                = var.linked_blob_storage_name
   resource_group_name = azurerm_resource_group.adf_resource_test.name
   data_factory_name   = azurerm_data_factory.adf_data_factory_test1.name
   connection_string   = azurerm_storage_account.adf_storage_account_test_1.primary_connection_string
@@ -55,7 +55,7 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "adf_linked_st
 
 # ADF blob dataset
 resource "azurerm_data_factory_dataset_azure_blob" "adf_dataset_test" {
-  name                = "${var.name}_dataset_test"
+  name                = var.dataset_blob_storage_name
   resource_group_name = azurerm_resource_group.adf_resource_test.name
   data_factory_name   = azurerm_data_factory.adf_data_factory_test1.name
   linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.adf_linked_storage_test.name
@@ -65,8 +65,29 @@ resource "azurerm_data_factory_dataset_azure_blob" "adf_dataset_test" {
 }
 
 # ADF Pipeline.
-resource "azurerm_data_factory_pipeline" "adf_pipeline_test"{
-	name = "${var.name}_pipeline_test"
-	resource_group_name = azurerm_resource_group.adf_resource_test.name
-	data_factory_name = azurerm_data_factory.adf_data_factory_test1.name
+resource "azurerm_data_factory_pipeline" "adf_pipeline_test1"{
+    name = var.pipeline_name
+    resource_group_name = azurerm_resource_group.adf_resource_test.name
+    data_factory_name = azurerm_data_factory.adf_data_factory_test1.name
+    variables = {
+        "temp" = "Item1"
+    }
+    # A JSON object that contains the activities that will 
+    # be associated with the Data Factory Pipeline.
+    activities_json =  <<JSON
+    [
+        {
+            "name": "Append to temp",
+            "type": "AppendVariable",
+            "dependsOn": [],
+            "userProperties": [],
+            "typeProperties": {
+                "variableName": "temp",
+                "value": "Item2"
+            }
+            
+        }
+    ]
+            
+    JSON   
 }
